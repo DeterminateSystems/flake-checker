85e3605c38fc1462a2138c941824c9e1ec0ce2a6
Graham Christensen <graham@grahamc.com>
2024-04-12 16:04:35 -0400

    Use DETSYS_CORRELATION if it is set

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -22,7 +22,9 @@ pub(crate) struct TelemetryReport {
 impl TelemetryReport {
     pub(crate) fn new(issues: &[Issue]) -> Result<TelemetryReport, env::VarError> {
         Ok(TelemetryReport {
-            distinct_id: calculate_opaque_id()?,
+            distinct_id: env::var("DETSYS_CORRELATION")
+                .ok()
+                .or_else(|| calculate_opaque_id().ok()),
 
             version: env!("CARGO_PKG_VERSION").to_string(),
             is_ci: is_ci::cached(),
