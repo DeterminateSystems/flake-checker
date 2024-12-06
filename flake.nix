332cbba8e859ae90596e394877a8600943cbac90
Luc Perkins <lucperkins@gmail.com>
2024-12-06 16:43:39 -0500

    Make build target depend on system

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
       forSystems = s: f: inputs.nixpkgs.lib.genAttrs s (system: f rec {
+        inherit system;
         pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [ self.overlays.default ];
@@ -53,7 +54,7 @@
           };
         };
 
-      packages = forAllSystems ({ pkgs, ... }: rec {
+      packages = forAllSystems ({ pkgs, system }: rec {
         default = flake-checker;
 
         flake-checker = pkgs.naerskLib.buildPackage
@@ -64,12 +65,18 @@
             buildInputs = with pkgs; [ ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [ Security SystemConfiguration ]);
             nativeBuildInputs = with pkgs; [ ] ++ lib.optionals stdenv.isDarwin [ libiconv ];
           } // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
-            CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+            CARGO_BUILD_TARGET =
+              if system == "x86_64-linux" then
+                "x86_64-unknown-linux-musl"
+              else if system == "aarch64-linux" then
+                "aarch64-unknown-linux-musl"
+              else
+                throw "Unsupported Linux system: ${system}";
             CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
           });
       });
 
-      devShells = forAllSystems ({ pkgs }: {
+      devShells = forAllSystems ({ pkgs, ... }: {
         default =
           let
             check-nixpkgs-fmt = pkgs.writeShellApplication {
